479c2ec1a05ff739be123e4f1ad8d4230ab9286a
Sergey <novoposarantu1@gmail.com>
2018-08-02 15:06:15 +0300

    New .cs files for authentication

---
+++ Domain/Concrete/UserContext.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+using System.Data.Entity;
+
+namespace Domain.Concrete
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
